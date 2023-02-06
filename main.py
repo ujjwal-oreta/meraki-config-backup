4f9841e558179d6cfb24670d35ddb34d233e81fb
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-02-06 13:32:02 +0530

    Uncommenting fetching config part

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from utils import blob_storage
 def main():
     organization_ids = [601793500207384061, 863973]
     print("Getting all Configurations")
-    return organization_ids
+    # return organization_ids
     config = configuration.get_all_configurations(organization_ids, is_cloud=True)
-    print(config)
-    configuration.store_all_configurations(config, organization_ids, is_cloud=True)
+    return config
+    # configuration.store_all_configurations(config, organization_ids, is_cloud=True)
     # blob_storage.dump_data('temp-location', {'Data': 'Temp Data'})
     return 200
 
