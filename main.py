0a889479e8e3777b33a713c36154bbc5c55780da
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-02-06 12:18:04 +0530

    Adding actions.yaml file

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import configuration 
 from utils import blob_storage
-import azure.functions as func
 
 
-def main(req: func.HttpRequest):
+def main():
     organization_ids = [601793500207384061, 863973]
     print("Getting all Configurations")
+    return organization_ids
     config = configuration.get_all_configurations(organization_ids, is_cloud=True)
     print(config)
     configuration.store_all_configurations(config, organization_ids, is_cloud=True)
     # blob_storage.dump_data('temp-location', {'Data': 'Temp Data'})
-    return func.HttpResponse(
-             "This HTTP triggered function executed successfully",
-             status_code=200
-        )
+    return 200
+
+
+if __name__ == "__main__":
+    main()
