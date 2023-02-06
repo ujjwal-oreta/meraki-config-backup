a427d037bbf85ec3890f4728d04442498fc1d0e9
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-02-06 16:05:36 +0530

    Storing BLOB_CONN_STR as Secret

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import os
 import configuration 
 from utils import blob_storage
 
 
 def main():
     organization_ids = [601793500207384061, 863973]
-    print("Getting all Configurations")
-    config = configuration.get_all_configurations(organization_ids, is_cloud=True)
-    configuration.store_all_configurations(config, organization_ids, is_cloud=True)
+    print(os.environ['BLOB_CONN_STR'])
+    # print("Getting all Configurations")
+    # config = configuration.get_all_configurations(organization_ids, is_cloud=True)
+    # configuration.store_all_configurations(config, organization_ids, is_cloud=True)
     # blob_storage.dump_data('temp-location', {'Data': 'Temp Data'})
     return 200
 
